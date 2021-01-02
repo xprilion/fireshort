e9ed801020ca91ec6fa83e1a68d2dd870a9a5b63
Lucky kumari <59636719+luckykumarirai@users.noreply.github.com>
2021-01-02 22:18:50 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByt9KDIBnpexFh09mwcBM9Q16N4HzuNdA",
-  authDomain: "fireshort-e4651.firebaseapp.com",
-  projectId: "fireshort-e4651",
-  storageBucket: "fireshort-e4651.appspot.com",
-  messagingSenderId: "918733614574",
-  appId: "1:918733614574:web:0327c33d341bcbf7bb32ff"
+  //your config value
 };
 
 
